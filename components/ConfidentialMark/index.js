0b74aee83d24a5125df52c6f4490315807ba0369
ykokw <0k4w4.yuk1@gmail.com>
2020-02-06 21:37:32 +0900

    styled example component

--- components/ConfidentialMark/index.js
+++ components/ConfidentialMark/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const ConfidentialMark = () => <span>Confidential</span>;
+const styleObject = {
+  position: "fixed",
+  top: 10,
+  right: 10,
+  padding: "4px",
+  border: "1px solid red",
+  color: "red",
+  fontSize: "2rem",
+  borderRadius: "8px",
+};
+
+const ConfidentialMark = () => <span style={styleObject}>Confidential</span>;
 
 export default ConfidentialMark;
